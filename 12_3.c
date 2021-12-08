54489976428284a983d81487f81f3743ed140f19
曹帅奇 <2466200050@qq.com>
2021-12-08 13:39:51 +0800

    测试啦

---
+++ 12_3.c
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c, d, e;
+//	int f, g, h, i, j;
+//	int k, l, m, n, o;
+//	scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+//	f = a / 3, g = (b + f) / 3, h = (c + g) / 3, i = (d + h) / 3, j = (e + f + i) / 3;
+//	k = f + j + g, l = g + h, m = h + i, n = i + j, o = j + 0;
+//	printf("%d %d %d %d %d\n", k, l, m, n, o);
+//	printf("%d", a % 3 + b % 3 + c % 3 + d % 3 + e % 3);
+//	return 0;
+//}

5586c814670c29c0499967454701fba3c21d60e8
曹帅奇 <2466200050@qq.com>
2021-12-28 21:39:18 +0800

    牛客练习

---
+++ 12_28.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+/*BC27 计算球体的体积 */
+//#define PI 3.1415926
+//#include<stdio.h>
+//int main()
+//{
+//	/*float类型精度不够，考的是double类型*/
+//	double r = 0;
+//	scanf("%lf", &r);
+//	double v = 0;
+//	v = (4 * PI * r * r * r) / 3;
+//	printf("%.3lf", v);
+//	return 0;
+//}
+/*BC38 变种水仙花 */
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int a = 0;
+//
+//	return 0;
+//}
+//12345
+//1 2345
+//12 345
+//123 45
+//1234 5

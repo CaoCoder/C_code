152bc2d0a2294a8aa44e5ade377af06ce2fd5607
曹帅奇 <2466200050@qq.com>
2022-01-15 22:10:16 +0800

    寒假作业1少一个编程题

---
+++ 2022_1_15.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+//c b acd c ab
+//#include<stdio.h>
+//int x = 5, y = 7;
+//void swap() 
+//{ 
+//	int z;
+//	z = x;
+//	x = y; 
+//	y = z; 
+//}
+//int main() 
+//{	int x = 3, y = 8; 
+//	swap();
+//	printf("%d,%d\n",x, y);
+//	return 0;
+//}
+/*打印从1到最大的n位数*/
+//#include<stdio.h>
+//static int arr[100010] = { 0 };
+//int* printNumbers(int n, int* returnSize)
+//{
+//	int k = 1;
+//	int j = 0;
+//	for (j = 0; j < n; j++)
+//	{
+//		k *= 10;
+//	}
+//	int i = 0;
+//	for (i = 1; i < k; i++)
+//	{
+//		*(arr + i - 1) = i;
+//	}
+//	*returnSize = i - 1;
+//	return arr;
+//}
+//returnSize是什么意思？
+

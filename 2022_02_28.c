e64231145723bcc891874205955eb3f7dafe0153
曹帅奇 <2466200050@qq.com>
2022-02-28 22:58:48 +0800

    阶段性失败测试

---
+++ 2022_02_28.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d", a);
+//	return 0;
+//}
+/*判断是斐波那契数列的最小步数*/
+//int main()
+//{
+//	int n = 0;
+//	int min = 0;
+//	scanf("%d", &n);
+//
+//	return 0;
+//}
+/*替换空格*/
+//#include<string.h>
+//void replaceSpace(char* str, int length)
+//{
+//	for (int i = 0; i < length; i++)
+//	{
+//		while (*str == ' ')
+//		{
+//			*str = '20';
+//		}
+//		str++;
+//	}
+//
+//}
+//int main()
+//{
+//	char arr[] = "we are happy.";
+//	int len = strlen(arr);
+//	replaceSpace(arr, len);
+//	printf("%s", arr);
+//	return 0;
+//}

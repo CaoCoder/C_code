d3fa45e51706195e2458ec978b5ef960080688a2
曹帅奇 <2466200050@qq.com>
2022-03-09 22:36:36 +0800

    0309.c

---
+++ 2022_03_09.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+//int main()
+//{
+//	float a[3] = { 1.5,2.5,3.5 };
+//	float* pa = a;
+//	float b = 0;
+//	/* b = *pa++;*/
+//	 b = *++pa;
+//
+//	printf("%f%f\n", *pa, b);
+
+
+	//float a[3] = { 1.5,2.5,3.5 };
+	//float* pa = a;
+	//*(pa++) *= 3;
+	//*pa++ *= 3;
+	//*(pa++) = *pa * 3;
+	//printf("%f\n", *pa);
+	//
+	//*pa++ *= 3;
+	//printf("%f\n", *pa);
+	//*(pa++) = *(pa++) * 3;
+	//printf("%f\n", *pa);
+	//*pa++ = *pa++ * 3;
+	//printf("%f", *pa);
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<assert.h>
+//char* my_strncat(char* dest, const char* src, size_t count)
+//{
+//	assert(dest && src);
+//	char* p = dest;
+//	while (*dest++)
+//	{
+//		;
+//	}
+//	while (count--)
+//	{
+//		*dest = *src;
+//		dest++;
+//		src++;
+//	}
+//	*dest = 0;
+//	return p;
+//}
+//int main()
+//{
+//	char arr[20] = "hello";
+//	char arr1[] = "world";
+//	char* p = my_strncat(arr, arr1, 3);
+//	printf("%s", p);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 3;
+//	while (a--)
+//	{
+//		;
+//	}
+//	printf("%d", a);
+//	return 0;
+//}

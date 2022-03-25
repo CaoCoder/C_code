2b75c758db81dba88e709ca482a74f815caf28f1
曹帅奇 <2466200050@qq.com>
2022-03-25 23:23:06 +0800

    栈的实现

---
+++ 栈的实现/Stack.h
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<assert.h>
+typedef int STDataType;
+
+typedef struct Stack
+{
+	STDataType* a;
+	int top;//’ª∂•Œª÷√
+	int capacity;
+}ST;
+void StackInit(ST* ps);
+void StackDestory(ST* ps);
+void StackPush(ST* ps, STDataType x);
+void StackPop(ST* ps);
+bool StackEmpty(ST* ps);
+STDataType StackTop(ST* ps);
+int StackSize(ST* ps);

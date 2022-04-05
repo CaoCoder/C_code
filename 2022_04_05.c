492ec5cf449aa0ebeefb053af674c1f97ec741b1
曹帅奇 <2466200050@qq.com>
2022-04-05 22:46:55 +0800

    d

---
+++ 2022_04_05.c
@@ -0,0 +1,14 @@
+//void eat() {
+//    int TYPE_SCORE[4] = { 10, 50, 10, 10 };
+//    print(f.location.x, f.location.y, 7, 0);
+//    mode = f.type;
+//    score += TYPE_SCORE[f.type];
+//    if (f.type == 1) {
+//        s.length *= 2;
+//    }
+//    else {
+//        s.length++;
+//    }
+//    make_food();
+//    return;
+//}

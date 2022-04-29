0e15b1018599aac4dd7aa2bf5072e5ea1e7251bc
曹帅奇 <2466200050@qq.com>
2022-04-29 09:13:33 +0800

    C++

---
+++ 2022_04_26.cpp
@@ -0,0 +1,49 @@
+//#include <stdio.h>
+//#include <iostream>
+//using namespace std;
+////namespace N 
+////{
+////	int a = 10;
+////	int b = 20;
+////	int Add(int left, int right)
+////	{
+////		return left + right;
+////	}
+////	int Sub(int left, int right)
+////	{
+////		return left - right;
+////	}
+////}
+////using namespace N;
+////int main()
+////{
+////	printf("%d\n", N:: a); // 该语句编译出错，无法识别a
+////	printf("%d\n", b);
+////	Add(10, 20);
+////	return 0;
+////}
+//
+////void TestFunc(int a = 0)
+////{
+////	std::cout << a;
+////}
+////void TestFunc(int a = 10, int b = 20, int c = 30)
+////{
+////	std::cout << "a = " << a << std::endl;
+////	cout << "b = "<< b << endl;
+////	cout << "c = "<< c << endl;
+////}
+//
+////在形参中如果形参有值，则说明这个函数叫做缺省参数。
+////缺省参数必须从右往左给出
+//void TestFunc(int a, int b = 10, int c = 20)
+//{
+//	cout << "a = " << a << endl;
+//	cout << "b = " << b << endl;
+//	cout << "c = " << c << endl;
+//}
+//int main()
+//{
+//	TestFunc(1,2,3);
+//	TestFunc(10);
+//}
